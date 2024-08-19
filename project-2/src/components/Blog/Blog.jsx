aac84deffa14d5a0324b4cf4d198e466cf872f7c
Nurunnahar <nurunnahar9081@gmail.com>
2024-08-19 12:43:54 +0600

    practice project-2

--- project-2/src/components/Blog/Blog.jsx
+++ project-2/src/components/Blog/Blog.jsx
@@ -1,15 +1,19 @@
-import  Single  from "./Single";
+import Single from "./Single";
+import { blogs } from "../../data/data";
 const Blog = () => {
   return (
     <>
       <div className="blog-area">
-        <Single
-          title="ঝুলে থাকা অবস্থায় পুলিশের ছয় গুলি, সেই তরুণ বেঁচে আছেন"
-          img="https://images.prothomalo.com/prothomalo-bangla%2F2024-08-18%2Frb9dmjzd%2FUntitled-12.jpg?rect=0%2C0%2C1134%2C756&auto=format%2Ccompress&fmt=webp&format=webp&w=640&dpr=1.0"
-          desc="নির্মাণাধীন একটি ভবনের চারতলার রড ধরে ঝুলে আছেন একজন তরুণ; তাঁকে লক্ষ্য করে পুলিশ গুলি করছে—সামাজিক যোগাযোগমাধ্যম ফেসবুকে এমন একটি ভিডিও ভাইরাল (ছড়িয়ে পড়া) হয়েছে।
-
-            সেই তরুণ বেঁচে আছেন। তাঁর নাম আমির হোসেন (১৮)। তিনি প্রথম আলোকে বলেছেন, ‘ঘুমের মধ্যে এখনো স্বপ্নে দেখি, পুলিশ আমাকে গুলি করছে।’"
-        />
+        {blogs.reverse().map((item) => {
+          return (
+            <Single
+              key={item.id}
+              title={item.title}
+              desc={item.desc}
+              img={item.img}
+            />
+          );
+        })}
       </div>
     </>
   );
