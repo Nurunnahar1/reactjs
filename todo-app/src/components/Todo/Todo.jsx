fedd4aac721ed05698a0cd9d7790295b446b9e97
Nurunnahar <nurunnahar9081@gmail.com>
2024-08-28 14:26:39 +0600

    todo app practice again

--- todo-app/src/components/Todo/Todo.jsx
+++ todo-app/src/components/Todo/Todo.jsx
@@ -2,42 +2,42 @@ import { useState } from "react";
 import "./Todo.css";
 
 const Todo = () => {
-    const [todo, setTodo] = useState(["go to school"]);
+    // show default input data
+    const [todo, setTodo] = useState(["Do Homework"])
 
     //catch input data
-    const [input, setInput] = useState("")
+    const [input, setInput] = useState("");
     const handleInputChange = (e) => {
         setInput(e.target.value);
     }
-    
+
     //add input data
     const handleTodoAdd = () => {
-        setTodo([input , ...todo]);
+        setTodo([input, ...todo]);
     }
-    
+
     //delete input data
     const handleTodoDelete = (item) => {
-        setTodo(todo.filter((data) => data != item));
+        setTodo(todo.filter((data) => { data != item }));
     }
-    
 
     return (
-      <>
-        <div className="todo">
-          <input type="text" placeholder="add todo" onChange={handleInputChange} value={input} />
-          <button className="add" onClick={handleTodoAdd}>Add</button>
-          <hr />
+        <>
+            <div className="todo">
+                <input type="text" placeholder="add todo" onChange={handleInputChange} value={input} />
+                <button className="add" onClick={handleTodoAdd} > Add</button>
                 <ul>
-                    {todo.map((item) => {
-                        return (
-                            <li key={item} >{item} <button onClick={ () => handleTodoDelete(item) } className="delete">x</button></li>
+                    {
+                        todo.map((item) => {
+                            return (
 
-                        );
-                    })}
-          </ul>
-        </div>
-      </>
+                                <li key={item}>{item} <button className="delete" onClick={() => handleTodoDelete(item)}>x</button></li>
+                            );
+                        })
+                    }
+                </ul>
+            </div>
+        </>
     );
 }
-
-export default Todo
+export default Todo;
