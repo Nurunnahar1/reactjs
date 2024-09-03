68893a6f346fbcaa0d43f49583cf751430424613
Nurunnahar <nurunnahar9081@gmail.com>
2024-09-03 14:53:51 +0600

    team member display from data.js

--- team-member-adding-dynamically/src/components/Team.jsx
+++ team-member-adding-dynamically/src/components/Team.jsx
@@ -1,42 +1,25 @@
-import './Team.css';
+import { useState } from "react";
+import "./Team.css";
+import { data } from "./data/data";
 
 const Team = () => {
-    return (
-      <div className="team">
-        <div className="single-item">
-          <img
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8R8QqmvD2GHfMgcUODxvVme8Cm6unpEDQ8w&s"
-            alt=""
-          />
-          <h2>Onamika</h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis
-            recusandae quos odit omnis nemo? Eius?
-          </p>
+  const [team, setTeam] = useState(data);
+  return (
+    <>
+      {team.length > 0 ? (  
+        <div className="team">
+          {team.map((item, index) => (
+            <div className="single-item" key={index}>
+              <img src={item.photo} alt={item.name} />
+              <h2>{item.name}</h2>
+              <p>{item.skill}</p>
+            </div>
+          ))}
         </div>
-        <div className="single-item">
-          <img
-            src="https://showcase.empower.net/wp-content/uploads/2019/08/team-member-02.jpg"
-            alt=""
-          />
-          <h2>Oporigita</h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis
-            recusandae quos odit omnis nemo? Eius?
-          </p>
-        </div>
-        <div className="single-item">
-          <img
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7ONFB4v5KvJXyvLG19IpJWcekBRaoQq04u0otxAHk-U0zbB4RMNjyLzdSOD2p6JaQCM0&usqp=CAU"
-            alt=""
-          />
-          <h2>Onupomuma</h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis
-            recusandae quos odit omnis nemo? Eius?
-          </p>
-        </div>
-      </div>
-    );
-}
+      ) : (
+        <p>No data found!</p>
+      )}
+    </>
+  );
+};
 export default Team;
---
+++ team-member-adding-dynamically/src/components/data/data.js
@@ -0,0 +1,26 @@
+export const data = [
+    {
+        id: 1,
+        name: "Onamika",
+        skill: "Lorem ipsum dolor sit amet consectetur adipisicing elit.Quis recusandae quos odit omnis nemo? Eius?",
+        photo:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8R8QqmvD2GHfMgcUODxvVme8Cm6unpEDQ8w&s"
+    },
+    {
+        id: 2,
+        name: "Oporigita",
+        skill: "Lorem ipsum dolor sit amet consectetur adipisicing elit.Quis recusandae quos odit omnis nemo? Eius?",
+        photo:"https://showcase.empower.net/wp-content/uploads/2019/08/team-member-02.jpg"
+    },
+    {
+        id: 3,
+        name: "Onupomuma",
+        skill: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis recusandae quos odit omnis nemo? Eius?",
+        photo:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7ONFB4v5KvJXyvLG19IpJWcekBRaoQq04u0otxAHk-U0zbB4RMNjyLzdSOD2p6JaQCM0&usqp=CAU"
+    },
+    {
+        id: 3,
+        name: "Onupomuma",
+        skill: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis recusandae quos odit omnis nemo? Eius?",
+        photo:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7ONFB4v5KvJXyvLG19IpJWcekBRaoQq04u0otxAHk-U0zbB4RMNjyLzdSOD2p6JaQCM0&usqp=CAU"
+    },
+]
