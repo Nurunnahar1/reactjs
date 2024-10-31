9490d4319797d6c111cc4c0a8c86342b59e9bf92
Nurunnahar <nurunnahar9081@gmail.com>
2024-10-31 23:31:14 +0600

    react crud create,update and data show done

--- react-crud/src/components/Student.jsx
+++ react-crud/src/components/Student.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 const Students = () => {
   //create student method
@@ -16,48 +17,159 @@ const Students = () => {
     }));
   };
 
+  const handelStudentFormSubmit = async (event) => {
+    event.preventDefault();
+    // console.log(input);
+    const response = await axios.post("http://localhost:5050/students", input);
+    setInput({
+      name: "",
+      roll: "",
+      email: "",
+      phone: "",
+      location: "",
+    });
+    getAllStudents();
+  };
+
+  //show student data
+  const [student, setStudent] = useState([]);
+
+  const getAllStudents = async () => {
+    const response = await axios.get("http://localhost:5050/students");
+    setStudent(response.data);
+  };
+
+  //update student data
+  const [updateForm, setUpdateForm] = useState(false);
+
+  const handleStudentEdit = async (id) => {
+    setUpdateForm(true);
+    const response = await axios.get(`http://localhost:5050/students/${id}`);
+    setInput(response.data);
+  };
+
+  //submit update student data
+  const handelStudentUpdateFormSubmit = async (event) => {
+    event.preventDefault();
+    const response = await axios.put(
+      `http://localhost:5050/students/${input.id}`,
+      {
+        name: input.name,
+        roll: input.roll,
+        email: input.email,
+        phone: input.phone,
+        location: input.location,
+      }
+    );
+    getAllStudents();
+    setInput({
+      name: "",
+      roll: "",
+      email: "",
+      phone: "",
+      location: "",
+    });
+      setUpdateForm(false);
+  };
+
+  useEffect(() => {
+    getAllStudents();
+  }, []);
+
   return (
     <>
       <div className="student-area">
         <div className="student-form">
-          <form action="">
-            <input
-              type="text"
-              placeholder="Name"
-              name="name"
-              value={input.name}
-              onChange={handleInputChange}
-            />
-            <input
-              type="text"
-              placeholder="Roll"
-              name="roll"
-              value={input.roll}
-              onChange={handleInputChange}
-            />
-            <input
-              type="text"
-              placeholder="Email"
-              name="email"
-              value={input.email}
-              onChange={handleInputChange}
-            />
-            <input
-              type="text"
-              placeholder="Phone"
-              name="phone"
-              value={input.phone}
-              onChange={handleInputChange}
-            />
-            <input
-              type="text"
-              placeholder="Location"
-              name="location"
-              value={input.location}
-              onChange={handleInputChange}
-            />
-            <button type="submit">Create New Student Data</button>
-          </form>
+          {!updateForm && (
+            <div className="create-new-data">
+              <h2>Create New Student Data</h2>
+              <form onSubmit={handelStudentFormSubmit}>
+                <input
+                  type="text"
+                  placeholder="Name"
+                  name="name"
+                  value={input.name}
+                  onChange={handleInputChange}
+                />
+                <input
+                  type="text"
+                  placeholder="Roll"
+                  name="roll"
+                  value={input.roll}
+                  onChange={handleInputChange}
+                />
+                <input
+                  type="text"
+                  placeholder="Email"
+                  name="email"
+                  value={input.email}
+                  onChange={handleInputChange}
+                />
+                <input
+                  type="text"
+                  placeholder="Phone"
+                  name="phone"
+                  value={input.phone}
+                  onChange={handleInputChange}
+                />
+                <input
+                  type="text"
+                  placeholder="Location"
+                  name="location"
+                  value={input.location}
+                  onChange={handleInputChange}
+                />
+                <button type="submit">Save</button>
+              </form>
+            </div>
+          )}
+
+          {updateForm && (
+            <div className="update-new-data">
+              <h2>Update Student Data</h2>
+              <button onClick={() => setUpdateForm(false)}>
+                Back to Create form
+              </button>
+              <form onSubmit={handelStudentUpdateFormSubmit}>
+                <input
+                  type="text"
+                  placeholder="Name"
+                  name="name"
+                  value={input.name}
+                  onChange={handleInputChange}
+                />
+                <input
+                  type="text"
+                  placeholder="Roll"
+                  name="roll"
+                  value={input.roll}
+                  onChange={handleInputChange}
+                />
+                <input
+                  type="text"
+                  placeholder="Email"
+                  name="email"
+                  value={input.email}
+                  onChange={handleInputChange}
+                />
+                <input
+                  type="text"
+                  placeholder="Phone"
+                  name="phone"
+                  value={input.phone}
+                  onChange={handleInputChange}
+                />
+                <input
+                  type="text"
+                  placeholder="Location"
+                  name="location"
+                  value={input.location}
+                  onChange={handleInputChange}
+                />
+                <button type="submit">Update</button>
+              </form>
+            </div>
+          )}
         </div>
         <div className="student-data">
           <table>
@@ -70,7 +182,30 @@ const Students = () => {
               <th>Location</th>
               <th>Action</th>
             </thead>
-            <tbody></tbody>
+            <tbody>
+              {student.map((item, index) => {
+                return (
+                  <tr key={item.id}>
+                    <td>{index + 1}</td>
+                    <td>{item.name}</td>
+                    <td>{item.roll}</td>
+                    <td>{item.email}</td>
+                    <td>{item.phone}</td>
+                    <td>{item.location}</td>
+                    <td>
+                      <button
+                        onClick={() => {
+                          handleStudentEdit(item.id);
+                        }}
+                      >
+                        Edit
+                      </button>
+                      <button>Delete</button>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
           </table>
         </div>
       </div>
